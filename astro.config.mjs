80f401232e25e0ec7065ad0a06996778573f95c9
rosemberg0212 <rosemberc.g.02@gmail.com>
2025-12-11 22:20:33 -0500

    base

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,13 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import partytown from '@astrojs/partytown';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), partytown()]
+  integrations: [react(), partytown()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
